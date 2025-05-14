d20f7a01b4471c92cd675ea547aa3b2bc7979057
musel25 <muselemmanuel@hotmail.com>
2025-05-14 20:30:00 +0200

    finish

--- 1-Contains Duplicate.py
+++ 1-Contains Duplicate.py
@@ -1,7 +1,10 @@
 from collections import Counter
 from typing import List 
 
-# 1st solution
+
+# My solutions
+
+# 1st solution using hashmap
 # class Solution:
 #     def hasDuplicate(self, nums: List[int]) -> bool:
 #         counter = Counter(nums)
@@ -16,13 +19,15 @@ from typing import List
 #     nums = [1, 2, 3, 1]
 #     print(solution.hasDuplicate(nums))  # Output: True
     
-# 2nd solution
+# 2nd solution using set
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        counter = Counter(nums)
-        for num, freq in counter.items():
-            if freq >1:
+        unique_nums = set()
+        for num in nums:
+            if num in unique_nums:
                 return True
+            else:
+                unique_nums.add(num)
 
         return False
 # Example usage
@@ -30,3 +35,20 @@ if __name__ == "__main__":
     solution = Solution()
     nums = [1, 2, 3, 1]
     print(solution.hasDuplicate(nums))  # Output: True
+
+
+# Extra solutions:
+
+# Sorting
+# class Solution:
+#     def hasDuplicate(self, nums: List[int]) -> bool:
+#         nums.sort()
+#         for i in range(1, len(nums)):
+#             if nums[i] == nums[i - 1]:
+#                 return True
+#         return False
+    
+# 4. Hash Set Length
+# class Solution:
+#     def hasDuplicate(self, nums: List[int]) -> bool:
+#         return len(set(nums)) < len(nums)
