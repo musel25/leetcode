f4914dd4a0d1e88acab8790c7d0bbaa80a535d05
musel25 <muselemmanuel@hotmail.com>
2025-05-19 12:46:20 +0200

    anagrams

---
+++ 4-Group Anagrams.py
@@ -0,0 +1,29 @@
+# Thoughts
+    # hasmap, where key equal to ordered word
+    # So for each word, we order it, then if not exist added to dict of anagrams as list.
+
+# Extra that I missed
+    # use defaultdict(list) to make it easier
+    # Hash Table
+
+# My solution 
+    # strs = ["act","pots","tops","cat","stop","hat"]
+
+    # dict_anagrams = {}
+    # for str in strs:
+    #     key = ''.join(sorted(str))
+    #     if key not in dict_anagrams:
+    #         dict_anagrams[key] = []
+    #     dict_anagrams[key].append(str)
+
+    # print(list(dict_anagrams.values()))
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
+        return list(res.values())
