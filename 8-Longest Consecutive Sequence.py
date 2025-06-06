d639dfd22604ae3fda7e11a5097776e0844ec636
musel25 <muselemmanuel@hotmail.com>
2025-06-06 09:31:38 +0200

    8

--- 8-Longest Consecutive Sequence.py
+++ 8-Longest Consecutive Sequence.py
@@ -6,25 +6,34 @@
     # Given we want only max value, then not need hasmap
 
 # Extra that I missed
-    # Hash set and map
+    # No need of both sorting and set. Only set enough, also can use hasmap
 
 
 
-nums=[0,3,2,5,4,6,1,1]
+nums=[0,3,2,5,4,6]
+
+# set_nums = set(nums)
+# sorted_set = sorted(set_nums)
+# curr = 1
+# len_sol  = 1
+# prev = sorted_set[0]
+# indices ={}
+# for i in range(1,len(sorted_set)):
+#     if sorted_set[i] == prev+1:
+#         curr +=1
+#         prev = sorted_set[i] 
+#     else:
+#         len_sol = max(len_sol, curr)
+#         curr = 1
+#         prev = sorted_set[i] 
+# print(max(len_sol, curr))
 
 set_nums = set(nums)
-sorted_set = sorted(set_nums)
-curr = 1
-len_sol  = 1
-prev = sorted_set[0]
-indices ={}
-for i in range(1,len(sorted_set)):
-    if sorted_set[i] == prev+1:
-        curr +=1
-        prev = sorted_set[i] 
-    else:
-        len_sol = max(len_sol, curr)
-        curr = 1
-        prev = sorted_set[i] 
-print(max(len_sol, curr))
-
+longest = 0
+for num in nums:
+    if num-1 not in set_nums:
+        length = 1
+        while (num + length) in set_nums:
+            length += 1
+        longest = max(length,longest)
+print(longest)
