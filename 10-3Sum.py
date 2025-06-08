98612cfd9110607cbc886aec74401e0e0a637cbe
musel25 <muselemmanuel@hotmail.com>
2025-06-08 15:15:20 +0200

    try

---
+++ 10-3Sum.py
@@ -0,0 +1,14 @@
+'''Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] where nums[i] + nums[j] + nums[k] == 0, and the indices i, j and k are all distinct.
+
+The output should not contain any duplicate triplets. You may return the output and the triplets in any order.'''
+
+# Thoughts
+    # I didnt come up with a solution aside from brute force.
+    
+# Extra that I missed
+
+nums = [-1,0,1,2,-1,-4]
+
+nums_sorted = sorted(nums)
+
+print(nums_sorted)
